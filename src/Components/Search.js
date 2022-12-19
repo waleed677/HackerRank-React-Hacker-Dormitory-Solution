df666c93db3fec4caa1fd7f607f58dbcbd17e486
Waleed Arshad Awan <waleedmalik677@gmail.com>
2022-12-19 15:11:44 +0500

    setup the project structure

---
+++ src/Components/Search.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+// `joiningDate` && `validityDate` format "yyyy-mm-dd"
+
+function checkValidity(joiningDate, validityDate) {
+  const now = new Date();
+  const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  const [year, month, day] = joiningDate.split("-");
+  const [yyyy, mm, dd] = validityDate.split("-");
+  const maxValid = new Date(yyyy, mm - 1, dd);
+  const selected = new Date(year, month - 1, day);
+  console.log(maxValid >= selected && maxValid >= today);
+  return maxValid >= selected && maxValid >= today;
+}
+
+
+function Search() {
+  
+  return (
+    <div className="my-50 layout-row align-items-end justify-content-end">
+      <label htmlFor="studentName">
+        Student Name:
+        <div>
+          <input
+            id="studentName"
+            data-testid="studentName"
+            type="text"
+            // value={name}
+            className="mr-30 mt-10"
+          />
+        </div>
+      </label>
+      <label htmlFor="joiningDate">
+        Joining Date:
+        <div>
+          <input
+            id="joiningDate"
+            data-testid="joiningDate"
+            type="date"
+            className="mr-30 mt-10"
+          />
+        </div>
+      </label>
+      <button
+        onClick={() => onAdd(residents, setResidents, setErrorText)}
+        type="button"
+        data-testid="addBtn"
+        className="small mb-0"
+      >
+        Add
+      </button>
+    </div>
+  );
+}
+
+export default Search;
